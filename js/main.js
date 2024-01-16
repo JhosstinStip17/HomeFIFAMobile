0851f1f65ecdf2e10b32d64afce72b0c88fa5cb6
Stip Vargas <141756797+JhosstinStip17@users.noreply.github.com>
2024-01-16 16:02:18 -0500

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  document.getElementById('reproducir').addEventListener('click', function() {
+    var audio = document.getElementById('miAudio');
+    audio.play();
+  });
+
+  document.getElementById('reproducir').addEventListener('click', function() {
+    var video = document.getElementById('miVideo');
+    video.play();
+  });
